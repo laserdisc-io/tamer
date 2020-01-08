916bbd216bd2debadb59c04048b8100eaf69c7f1
Julien Sirocchi <julien.sirocchi@gmail.com>
2020-01-08 18:44:17 +0000

    make the example work again

--- example/scripts/run.sh
+++ example/scripts/run.sh
@@ -6,6 +6,7 @@ export DATABASE_URL=jdbc:postgresql://localhost:5432/postgres
 export DATABASE_USERNAME=postgres
 export DATABASE_PASSWORD=mysecretpassword
 export KAFKA_BROKERS=localhost:9092
+export KAFKA_SCHEMA_REGISTRY_URL=http://localhost:8081
 export KAFKA_CLOSE_TIMEOUT="30 seconds"
 export KAFKA_BUFFER_SIZE=4096
 export KAFKA_SINK_TOPIC=sink-topic
