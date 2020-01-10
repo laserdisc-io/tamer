b99e78b781ebf0c15734d97cbf5491a6cd29c431
Amir Saeid <amir.saeid@hbc.com>
2020-01-10 10:58:40 +0000

    Replace Task with IO
    Also preserve TamerError

--- core/src/main/scala/tamer/errors.scala
+++ core/src/main/scala/tamer/errors.scala
@@ -1,7 +1,14 @@
 package tamer
 
-sealed abstract class TamerError(msg: String)    extends RuntimeException(msg)
-final case class ConfigError(msg: String)        extends TamerError(msg)
-final case class KafkaError(msg: String)         extends TamerError(msg)
-final case class DbError(msg: String)            extends TamerError(msg)
-final case class SerializationError(msg: String) extends TamerError(msg)
+sealed abstract class TamerError(msg: String, cause: Throwable)    extends RuntimeException(msg, cause)
+final case class ConfigError(msg: String, cause: Throwable)        extends TamerError(msg, cause)
+final case class KafkaError(msg: String, cause: Throwable)         extends TamerError(msg, cause)
+final case class DbError(msg: String, cause: Throwable)            extends TamerError(msg, cause)
+final case class SerializationError(msg: String, cause: Throwable) extends TamerError(msg, cause)
+final case class SetupError(msg: String, cause: Throwable)         extends TamerError(msg, cause)
+
+object ConfigError        { final def apply(msg: String): ConfigError        = new ConfigError(msg, null)        }
+object KafkaError         { final def apply(msg: String): KafkaError         = new KafkaError(msg, null)         }
+object DbError            { final def apply(msg: String): DbError            = new DbError(msg, null)            }
+object SerializationError { final def apply(msg: String): SerializationError = new SerializationError(msg, null) }
+object SetupError         { final def apply(msg: String): SetupError         = new SetupError(msg, null)         }
