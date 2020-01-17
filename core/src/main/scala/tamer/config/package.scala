b5f9e5e1d352622901bc8269370be16d656e6a7b
Amir Saeid <amir.saeid@hbc.com>
2020-01-17 11:39:13 +0000

    Unrefine password

--- core/src/main/scala/tamer/config/package.scala
+++ core/src/main/scala/tamer/config/package.scala
@@ -7,7 +7,7 @@ import eu.timepit.refined.string.{IPv4, Uri, Url}
 
 package object config {
   type HostList  = List[String] Refined (NonEmpty And Forall[IPv4 Or Uri])
-  type Password  = String Refined NonEmpty
+  type Password  = String
   type UriString = String Refined Uri
   type UrlString = String Refined Url
 }
