124b447d6035e97f3cb8ec633baf01c3f1453943
Pavel Shirshov <pshirshov@gmail.com>
2021-03-05 14:54:13 +0000

    db#fetch converted into job

--- doobie/src/main/scala/tamer/db/package.scala
+++ doobie/src/main/scala/tamer/db/package.scala
@@ -1,26 +1,16 @@
 package tamer
 
 import cats.effect.Blocker
-import com.sksamuel.avro4s.Codec
-import doobie.Query0
 import doobie.hikari.HikariTransactor
-import doobie.implicits.{toDoobieStreamOps, _}
 import doobie.util.transactor.Transactor
 import eu.timepit.refined.auto._
-import fs2.Stream
-import log.effect.LogWriter
-import log.effect.zio.ZioLogWriter.log4sFromName
-import tamer.config.{Config, KafkaConfig}
-import tamer.db.Compat.toIterable
 import tamer.db.ConfigDb.{DbConfig, QueryConfig}
-import tamer.kafka.Kafka
 import zio.blocking.Blocking
-import zio.clock.Clock
 import zio.interop.catz._
-import zio.{Queue, Task, UIO, ZIO, _}
+import zio.{Task, ZIO, _}
 
 import java.sql.SQLException
-import java.time.{Duration, Instant}
+import java.time.Instant
 import scala.concurrent.ExecutionContext
 
 package object db {
@@ -35,59 +25,7 @@ package object db {
       }
   }
 
-  private[this] final val logTask: Task[LogWriter[Task]] = log4sFromName.provide("tamer.db")
 
-  private final def iteration[K <: Product, V <: Product, S <: Product: HashableState](
-      setup: DoobieConfiguration[K, V, S]
-  )(state: S, q: Queue[Chunk[(K, V)]]): ZIO[TamerDBConfig, TamerError, S] =
-    (for {
-      log   <- logTask
-      cfg   <- ConfigDb.queryConfig
-      tnx   <- ZIO.service[Transactor[Task]]
-      query <- UIO(setup.queryBuilder.query(state))
-      _     <- log.debug(s"running ${query.sql} with params derived from $state")
-      start <- UIO(Instant.now())
-      values <-
-        query
-          .streamWithChunkSize(cfg.fetchChunkSize)
-          .chunks
-          .transact(tnx)
-          .map(ChunkWithMetadata(_))
-          .evalTap(c => q.offer(Chunk.fromIterable(toIterable(c.chunk).map(v => setup.keyExtract(v) -> v))))
-          .flatMap(c => Stream.chunk(c.chunk).map(ValueWithMetadata(_, c.pulledAt)))
-          .compile
-          .toList
-      pulledTimeOrNow = values.headOption.map(_.pulledAt).getOrElse(Instant.now())
-      newState <- setup.stateFoldM(state)(
-        QueryResult(
-          ResultMetadata(
-            Duration.between(start, pulledTimeOrNow).toMillis
-          ),
-          values.map(_.value)
-        )
-      )
-    } yield newState).mapError(e => TamerError(e.getLocalizedMessage, e))
-
-  final def fetchWithTimeSegment[K <: Product: Codec, V <: Product with Timestamped: Ordering: Codec](
-      queryBuilder: TimeSegment => Query0[V]
-  )(earliest: Instant, tumblingStep: Duration, keyExtract: V => K): ZIO[ZEnv, TamerError, Unit] = {
-    val transactorLayer: Layer[TamerError, DbTransactor]               = (Blocking.live ++ ConfigDb.live) >>> db.hikariLayer
-    val queryConfigLayer: Layer[TamerError, DbConfig with QueryConfig] = ConfigDb.live
-    val setup: DoobieConfiguration[K, V, TimeSegment]                  = DoobieConfiguration.fromTimeSegment[K, V](queryBuilder)(earliest, tumblingStep, keyExtract)
-    fetch(setup).provideSomeLayer[ZEnv](transactorLayer ++ queryConfigLayer ++ Config.live)
-  }
-
-  final def fetch[
-      K <: Product: Codec,
-      V <: Product: Codec,
-      S <: Product: Codec: HashableState
-  ](
-      setup: DoobieConfiguration[K, V, S]
-  ): ZIO[ZEnv with DbConfig with TamerDBConfig with KafkaConfig, TamerError, Unit] = {
-    val kafkaLayer: ZLayer[TamerDBConfig with KafkaConfig with Clock with Blocking, TamerError, Kafka] =
-      Kafka.live(setup.generic, iteration(setup))
-    tamer.kafka.runLoop.provideSomeLayer[ZEnv with DbConfig with TamerDBConfig with KafkaConfig](kafkaLayer)
-  }
 
   final val hikariLayer: ZLayer[Blocking with DbConfig, TamerError, DbTransactor] = ZLayer.fromManaged {
     for {
