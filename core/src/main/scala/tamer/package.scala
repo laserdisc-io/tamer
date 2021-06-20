35310d0bf17e20fa6eb566809d47fb0a04eda30e
Julien Sirocchi <julien.sirocchi@gmail.com>
2021-06-20 21:46:17 +0100

    a little less Blocking

--- core/src/main/scala/tamer/package.scala
+++ core/src/main/scala/tamer/package.scala
@@ -1,13 +1,12 @@
 import zio.{Has, Layer, URIO, ZIO}
-import zio.blocking.Blocking
 import zio.clock.Clock
 
 package object tamer {
   final type RegistryInfo = Has[Registry] with Has[TopicName]
   final type TopicName    = String
 
-  final val kafkaConfig: URIO[Has[KafkaConfig], KafkaConfig]                    = ZIO.service
-  final val runLoop: ZIO[Has[Tamer] with Blocking with Clock, TamerError, Unit] = ZIO.accessM(_.get.runLoop)
+  final val kafkaConfig: URIO[Has[KafkaConfig], KafkaConfig]      = ZIO.service
+  final val runLoop: ZIO[Has[Tamer] with Clock, TamerError, Unit] = ZIO.accessM(_.get.runLoop)
 
   final type ZSerde[-R, T] = zio.kafka.serde.Serde[R, T]
   final val ZSerde = zio.kafka.serde.Serde
