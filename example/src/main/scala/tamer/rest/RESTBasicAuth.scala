9ad29614832db52ab319d7945b7b35b86218fba7
Amir Saeid <amir@glgdgt.com>
2021-06-30 15:32:46 +0100

    [rest] Add retry schedule

--- example/src/main/scala/tamer/rest/RESTBasicAuth.scala
+++ example/src/main/scala/tamer/rest/RESTBasicAuth.scala
@@ -1,7 +1,9 @@
 package tamer
 package rest
 
+import sttp.client3.{Response, RetryWhen}
 import zio._
+import zio.duration._
 
 object RESTBasicAuth extends App {
   case class MyKey(i: Int)
@@ -14,11 +16,17 @@ object RESTBasicAuth extends App {
       case pageBody        => Task.fail(new RuntimeException(s"Could not parse pageBody: $pageBody"))
     }
 
+  def retrySchedule(
+      request: SttpRequest
+  ): Schedule[Any, Either[Throwable, Response[Either[String, String]]], Either[Throwable, Response[Either[String, String]]]] =
+    Schedule.spaced(5.seconds) *> Schedule.recurs(3) *> Schedule.recurWhile(response => RetryWhen.Default(request, response))
+
   val program: ZIO[ZEnv, TamerError, Unit] = RESTSetup
     .paginated(
       baseUrl = "http://localhost:9095/basic-auth",
       pageDecoder = pageDecoder,
-      authentication = Some(Authentication.basic("user", "pass"))
+      authentication = Some(Authentication.basic("user", "pass")),
+      retrySchedule = Some(retrySchedule)
     )(
       recordKey = (_, data) => MyKey(data.i),
       offsetParameterName = "offset",
